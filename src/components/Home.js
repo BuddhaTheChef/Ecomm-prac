00678126a740ed16415132a16f272cafc2c688cb
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-01-20 18:44:53 -0800

    changed styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ class Home extends Component {
         return (
             <div className="app-header">
             <div style={{background: 'whitesmoke', height: '100px', display: 'flex', alignItems: 'center', width: 'inherit', marginLeft: '61px', color: '#282c34'}}>
-            <h1 style={{ margin: '0'}}>Shop-if-why</h1>
+            <h1 style={{marginLeft: '50px'}}>Shop-if-why</h1>
             </div>
             <div style={{display: 'flex', height: '89%',marginLeft: '60px'}}>
           
