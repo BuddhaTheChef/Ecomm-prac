677bdc8bbba316d16d44bed292d1e43e65886813
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-02-04 17:03:36 -0800

    added in checkout functionality

--- src/context.js
+++ src/context.js
@@ -52,7 +52,7 @@ class ProductProvider extends Component {
         product.total = price;
         this.setState(()=> {
             return {products: tempProducts, cart:[...this.state.cart, product]}
-        }, ()=>{console.log(this.state)})
+        }, ()=>{this.addTotals()})
     }
 
     openModal = id => {
@@ -77,11 +77,42 @@ class ProductProvider extends Component {
     }
 
     removeItem = (id) => {
-        console.log('removed item ')
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+        tempCart = tempCart.filter(item => item.id !== id);
+
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        this.setState({
+            cart: [...tempCart], 
+            products: [...tempProducts]
+        },
+        ()=>{this.addTotals()})
     }
 
     clearCart = () => {
-        console.log('cart was cleared')
+        this.setState({cart:[]},
+            ()=> {
+                this.setProducts();
+                this.addTotals();
+        })
+    }
+
+    addTotals = () => {
+        let subTotal = 0;
+        this.state.cart.map(item => (subTotal += item.total));
+        const tempTax = subTotal * 0.1;
+        const tax = parseFloat(tempTax.toFixed(2));
+        const total = subTotal + tax;
+        this.setState({
+                cartSubTotal: subTotal,
+                cartTax: tax,
+                cartTotal: total
+            })
     }
 
 
